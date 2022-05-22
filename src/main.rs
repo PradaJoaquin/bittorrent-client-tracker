f156210ab5a6c7bad6e39c08e40e2375b83abe86
Joaquin Prada <36085103+PradaJoaquin@users.noreply.github.com>
2022-05-22 17:20:31 -0300

    feature/argument-parser (#31)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!(
+            "Wrong number of arguments: {}. Only a directory path containing one or more torrents should be passed",
+            (args.len() - 1)
+        );
+    }
+
+    //bt_client::init(args[1])
 }
