be507a17d5c84bf0ca7b5cef3d2eefc7cd88ad0a
NContinanza <ncontinanza@fi.uba.ar>
2022-05-28 15:29:30 -0300

    Refactor tests to allow multithreaded execution

--- src/storage_manager/manager.rs
+++ src/storage_manager/manager.rs
@@ -24,64 +24,69 @@ mod tests {
 
     #[test]
     fn save_file_creates_file_if_it_does_not_exist() {
-        assert!(!Path::new("test_file.txt").exists());
+        let path = "test_file_01.txt";
+        assert!(!Path::new(path).exists());
         assert!(save_piece(
-            "test_file.txt".to_string(),
+            path.to_string(),
             &[0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8],
             0
         )
         .is_ok());
-        assert!(Path::new("test_file.txt").exists());
-        remove_file("test_file.txt").unwrap();
+        assert!(Path::new(path).exists());
+        remove_file(path).unwrap();
     }
 
     #[test]
     fn write_in_nonexistent_file() {
-        assert!(!Path::new("test_file.txt").exists());
+        let path = "test_file_02.txt";
+        assert!(!Path::new(path).exists());
 
         let content_to_write = vec![0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8];
-        assert!(save_piece("test_file.txt".to_string(), &content_to_write, 0).is_ok());
-        assert!(Path::new("test_file.txt").exists());
+        assert!(save_piece(path.to_string(), &content_to_write, 0).is_ok());
+        assert!(Path::new(path).exists());
 
-        read_file_and_assert_its_content_equals_expected_content(content_to_write, "test_file.txt");
+        read_file_and_assert_its_content_equals_expected_content(content_to_write, path);
 
-        remove_file("test_file.txt").unwrap();
+        remove_file(path).unwrap();
     }
 
     #[test]
     fn write_in_existing_file() {
-        File::create("test_file.txt").unwrap();
+        let path = "test_file_03.txt";
+        File::create(path).unwrap();
 
         let content_to_write = vec![0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8];
-        assert!(save_piece("test_file.txt".to_string(), &content_to_write, 0).is_ok());
+        assert!(save_piece(path.to_string(), &content_to_write, 0).is_ok());
 
-        read_file_and_assert_its_content_equals_expected_content(content_to_write, "test_file.txt");
+        read_file_and_assert_its_content_equals_expected_content(content_to_write, path);
 
-        remove_file("test_file.txt").unwrap();
+        remove_file(path).unwrap();
     }
 
     #[test]
     fn write_at_the_end_of_existing_file_that_already_has_contents() {
-        let mut file = File::create("test_file.txt").unwrap();
+        let path = "test_file_04.txt";
+        let mut file = File::create(path).unwrap();
         let previous_content = vec![0x56u8, 0x69u8, 0x76u8, 0x61u8, 0x20u8];
         file.write_all(&previous_content).unwrap();
 
         let content_to_write = vec![0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8];
-        assert!(save_piece("test_file.txt".to_string(), &content_to_write, 5).is_ok());
+        assert!(save_piece(path.to_string(), &content_to_write, 5).is_ok());
 
         read_file_and_assert_its_content_equals_expected_content(
             vec![
                 0x56u8, 0x69u8, 0x76u8, 0x61u8, 0x20u8, 0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8,
             ],
-            "test_file.txt",
+            path,
         );
 
-        remove_file("test_file.txt").unwrap();
+        remove_file(path).unwrap();
     }
 
     #[test]
     fn write_between_pieces_of_existing_file_that_already_has_contents() {
-        let mut file = File::create("test_file.txt").unwrap();
+        let path = "test_file_05.txt";
+        let mut file = File::create(path).unwrap();
         let first_piece = vec![0x56u8, 0x69u8, 0x76u8, 0x61u8];
         let second_piece = vec![0x20, 0x50u8, 0x65u8];
         let third_piece = vec![0x72u8, 0xF3u8, 0x6Eu8];
@@ -89,16 +94,16 @@ mod tests {
         file.write_all(&first_piece).unwrap();
         file.write_all_at(&third_piece, 7).unwrap();
 
-        assert!(save_piece("test_file.txt".to_string(), &second_piece, 4).is_ok());
+        assert!(save_piece(path.to_string(), &second_piece, 4).is_ok());
 
         read_file_and_assert_its_content_equals_expected_content(
             vec![
                 0x56u8, 0x69u8, 0x76u8, 0x61u8, 0x20u8, 0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8,
             ],
-            "test_file.txt",
+            path,
         );
 
-        remove_file("test_file.txt").unwrap();
+        remove_file(path).unwrap();
     }
 
     fn read_file_and_assert_its_content_equals_expected_content(
