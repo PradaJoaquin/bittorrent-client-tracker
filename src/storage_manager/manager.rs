4703ccfdca7ff1bf793e4769c94de08abfe35831
NContinanza <ncontinanza@fi.uba.ar>
2022-05-28 15:22:23 -0300

    Add test and refactor repeating code

--- src/storage_manager/manager.rs
+++ src/storage_manager/manager.rs
@@ -43,8 +43,8 @@ mod tests {
         assert!(save_piece("test_file.txt".to_string(), &content_to_write, 0).is_ok());
         assert!(Path::new("test_file.txt").exists());
 
-        let content = read("test_file.txt").unwrap();
-        assert_eq!(content, content_to_write);
+        read_file_and_assert_its_content_equals_expected_content(content_to_write, "test_file.txt");
+
         remove_file("test_file.txt").unwrap();
     }
 
@@ -55,25 +55,57 @@ mod tests {
         let content_to_write = vec![0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8];
         assert!(save_piece("test_file.txt".to_string(), &content_to_write, 0).is_ok());
 
-        let content = read("test_file.txt").unwrap();
-        assert_eq!(content, content_to_write);
+        read_file_and_assert_its_content_equals_expected_content(content_to_write, "test_file.txt");
+
         remove_file("test_file.txt").unwrap();
     }
 
     #[test]
-    fn write_in_existing_file_that_already_has_contents() {
+    fn write_at_the_end_of_existing_file_that_already_has_contents() {
         let mut file = File::create("test_file.txt").unwrap();
-        let previous_content = vec![0x56, 0x69, 0x76, 0x61, 0x20];
+        let previous_content = vec![0x56u8, 0x69u8, 0x76u8, 0x61u8, 0x20u8];
         file.write_all(&previous_content).unwrap();
 
         let content_to_write = vec![0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8];
         assert!(save_piece("test_file.txt".to_string(), &content_to_write, 5).is_ok());
 
-        let content = read("test_file.txt").unwrap();
-        let expected_content = vec![
-            0x56, 0x69, 0x76, 0x61, 0x20, 0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8,
-        ];
-        assert_eq!(content, expected_content);
+        read_file_and_assert_its_content_equals_expected_content(
+            vec![
+                0x56u8, 0x69u8, 0x76u8, 0x61u8, 0x20u8, 0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8,
+            ],
+            "test_file.txt",
+        );
+
         remove_file("test_file.txt").unwrap();
+    }
+
+    #[test]
+    fn write_between_pieces_of_existing_file_that_already_has_contents() {
+        let mut file = File::create("test_file.txt").unwrap();
+        let first_piece = vec![0x56u8, 0x69u8, 0x76u8, 0x61u8];
+        let second_piece = vec![0x20, 0x50u8, 0x65u8];
+        let third_piece = vec![0x72u8, 0xF3u8, 0x6Eu8];
+
+        file.write_all(&first_piece).unwrap();
+        file.write_all_at(&third_piece, 7).unwrap();
+
+        assert!(save_piece("test_file.txt".to_string(), &second_piece, 4).is_ok());
+
+        read_file_and_assert_its_content_equals_expected_content(
+            vec![
+                0x56u8, 0x69u8, 0x76u8, 0x61u8, 0x20u8, 0x50u8, 0x65u8, 0x72u8, 0xF3u8, 0x6Eu8,
+            ],
+            "test_file.txt",
+        );
+
+        remove_file("test_file.txt").unwrap();
+    }
+
+    fn read_file_and_assert_its_content_equals_expected_content(
+        expected_content: Vec<u8>,
+        file_name: &str,
+    ) {
+        let content = read(file_name).unwrap();
+        assert_eq!(content, expected_content);
     }
 }
