b373d592d425f6d0976e5326e4f793ad019da08f
Joaquin Prada <36085103+PradaJoaquin@users.noreply.github.com>
2022-07-30 17:22:11 -0300

    feature/max days to keep stats (#159)
    maximum implemented

--- dtracker/src/stats/stats_updater.rs
+++ dtracker/src/stats/stats_updater.rs
@@ -7,6 +7,9 @@ use logger::logger_sender::LoggerSender;
 use crate::tracker_status::atomic_tracker_status::AtomicTrackerStatus;
 use crate::tracker_status::current_tracker_stats::CurrentTrackerStats;
 
+// for 1 month it takes 0.5 miliseconds to update the stats. And 0.5 Megabytes to store the stats.
+const MAX_DAYS_TO_KEEP_STATS: u64 = 30;
+
 /// Struct that represents the current status of the stats.
 #[derive(Debug)]
 pub struct StatsUpdater {
@@ -35,8 +38,16 @@ impl StatsUpdater {
     pub fn run(&self) {
         loop {
             self.tracker_status.remove_inactive_peers();
-            self.lock_stats_history()
-                .push(self.tracker_status.get_global_statistics());
+            let mut stats_history = self.lock_stats_history();
+
+            // If we reached the maximum number of days to keep stats, remove the oldest one.
+            let max_secs_to_keep_stats = MAX_DAYS_TO_KEEP_STATS * 24 * 60 * 60;
+            if self.duration.as_secs() * stats_history.len() as u64 > max_secs_to_keep_stats {
+                stats_history.rotate_left(1);
+                stats_history.pop();
+            }
+
+            stats_history.push(self.tracker_status.get_global_statistics());
             self.lock_logger_sender().info("Stats updated");
             sleep(self.duration);
         }
